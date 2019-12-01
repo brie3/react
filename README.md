abb587dfd04b2ff0887ecc22bab29cbfec0e480d
brie3 <b3r6i9@gmail.com>
2019-12-01 23:58:14 +0500

    some code added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React scripts.
 <p>
 
-[installation](scripts/installation.html) - Given an age in seconds, calculate how old someone would be on: Earth, Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune.
+[installation](scripts/installation.html) - Install node and npm. Create a project and configure package.json. Set up webpack.config. Using webpack, build the simplest React code using the functional component of the message. Issue the html file into which the assembly is connected using webpack-dev-server. Implement the ability to send a new message with fixed text: add button; process a button click with a function that adds a new message to the message array - for example, with the text “Normal”. The handler function should also do a DOM’s renderer so that the new message is not just added to the array, but also appears on the page of the user. ### All problems and ways to solve them will be discussed in detail at the next lesson.
 </p>
--- scripts/installation/src/index.js
+++ scripts/installation/src/index.js
@@ -1,5 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+const messages = [
+    {
+        id: 0,
+        name: "Vasia Pupkine",
+        content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+    },
+    {
+        id: 1,
+        name: "Doloremque",
+        content: "In hac habitasse platea dictumst. Sed quis eros suscipit, tristique augue quis"
+    },
+    {
+        id: 2,
+        name: "Voluptatum",
+        content: "Rerum totam dicta error, doloremque officiis rem molestias asperiores cupiditate,"
+    },
+    {
+        id: 3,
+        name: "Aspernatur",
+        content: "Veritatis aliquam eaque provident voluptatum fuga?"
+    },
+    {
+        id: 4,
+        name: "Velit",
+        content: "Velit quia id omnis incidunt fugit dolores hic, aperiam perspiciatis quidem natus."
+    }
+]
 
-ReactDOM.render(elementWithJSX, document.getElementById("root"))
+const Message = (props) => <div><b>{props.name}</b>: {props.content}</div>
+const MessageList = function(props){
+    return props.messages.map(item => <Message name={item.name} content={item.content} key={item.id}/>)
+}
+ReactDOM.render(<MessageList messages={messages} />, document.getElementById("root"))
