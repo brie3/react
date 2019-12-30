0100da0a5960b5f314e5a70bc13a5d6b78082b8a
brie3 <b3r6i9@gmail.com>
2019-12-30 13:25:24 +0500

    service worker done ready for ubuntu test

--- src/middlewares/chatMiddleware.js
+++ src/middlewares/chatMiddleware.js
@@ -6,33 +6,33 @@ import { LOCATION_CHANGE } from "connected-react-router";
 var storedChatID = {};
 const chatMiddleware = state => next => action => {
     switch (action.type) {
-        case ADD_CHAT:
+    case ADD_CHAT:
+        createChat(action);
+        break;
+    case SEND_MESSAGE:
+        if (!(action.chatID in state.getState().chatReducer.chats)) {
             createChat(action);
-            break;
-        case SEND_MESSAGE:
-            if (!(action.chatID in state.getState().chatReducer.chats)) {
-                createChat(action);
-            }
+        }
+        setTimeout(() => {
+            state.dispatch(showNotice(action.chatID));
+            storedChatID[action.chatID] = action.chatID;
             setTimeout(() => {
-                state.dispatch(showNotice(action.chatID));
-                storedChatID[action.chatID] = action.chatID;
-                setTimeout(() => {
-                    if (
-                        state.getState().router.location.pathname ===
+                if (
+                    state.getState().router.location.pathname ===
                         "/chats/" + action.chatID
-                    ) {
-                        state.dispatch(hideNotice(action.chatID));
-                    }
-                }, 2000);
-            }, 1000);
-            break;
-        case LOCATION_CHANGE:
-            const addr = action.payload.location.pathname.split("/")[2];
-            if (storedChatID[addr] && storedChatID[addr] == addr) {
-                state.dispatch(hideNotice(storedChatID[addr]));
-                delete storedChatID[addr];
-            }
-            break;
+                ) {
+                    state.dispatch(hideNotice(action.chatID));
+                }
+            }, 2000);
+        }, 1000);
+        break;
+    case LOCATION_CHANGE:
+        const addr = action.payload.location.pathname.split("/")[2];
+        if (storedChatID[addr] && storedChatID[addr] == addr) {
+            state.dispatch(hideNotice(storedChatID[addr]));
+            delete storedChatID[addr];
+        }
+        break;
     }
     return next(action);
 };
